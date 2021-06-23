b92e8f3c8c3a932be3c0e71df01a2dd25a1b5c04
Federico Peruzzi <federico.xu125@gmail.com>
2021-06-23 17:45:32 +0200

    added footer and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,61 @@ export class AppComponent implements AfterViewInit {
         observer.observe(element);
       });
     }
+
+    let anchors = [
+      document.querySelector('#work'),
+      document.querySelector('#resume'),
+      document.querySelector('#about-me'),
+      document.querySelector('#contact')
+    ];
+
+    let menuPath = [
+      { key: "work", element: document.querySelector('#side-nav-work .path-side-menu') },
+      { key: "resume", element: document.querySelector('#side-nav-resume .path-side-menu') },
+      { key: "about-me", element: document.querySelector('#side-nav-about-me .path-side-menu') },
+      { key: "contact", element: document.querySelector('#side-nav-contact .path-side-menu') }
+    ];
+
+    const observerAnchor = new IntersectionObserver(entries => {
+      let shouldAnimate = "";
+      anchors.forEach(anch => {
+        if (anch && this.checkVisible(anch)) {
+          shouldAnimate = anch.id;
+        }
+      });
+
+      let elementToAnimate = menuPath.find(elem => { return elem.key == shouldAnimate })?.element
+      if (elementToAnimate && !elementToAnimate.classList.contains("path-animation-side-menu")) {
+        this.removeAnimation(menuPath);
+        this.addAnimation(elementToAnimate);
+      }
+
+    });
+
+    anchors.forEach(element => {
+      if (element)
+        observerAnchor.observe(element);
+    });
+
   }
 
+  removeAnimation(menuPath: any) {
+    menuPath.forEach((elem: any) => {
+      if (elem.element) {
+        elem.element.classList.remove('path-animation-side-menu');
+        elem.element.classList.add('display-none');
+      }
+    });
+  }
+
+  addAnimation(menuPath: any) {
+    menuPath.classList.add('path-animation-side-menu');
+    menuPath.classList.remove('display-none');
+  }
+
+  checkVisible(elm: any) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+  }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'menu',
@@ -10,6 +11,28 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var th = this;
+    $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+      $('.name').css({
+        opacity: function () {
+          return th.opacity($(this).height(), scrollTop, 150);
+        }
+      });
+      $('.side-nav').css({
+        opacity: function () {
+          return  th.opacity($(this).height(), scrollTop, 200) ;
+        }
+      });
+    });
+  }
+
+  opacity(height: number, top: number, startPx: number){
+    return 1- this.numberMap(top, height, startPx, 0, 1);
+  }
+
+  numberMap (input:number,in_min: number, in_max: number, out_min: number, out_max: number) {
+    return (input - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
 
 }
